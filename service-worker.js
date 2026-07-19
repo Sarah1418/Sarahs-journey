459df2560cc7f62b955df10a6f58a8dbf813c928
Sarah1418 <salinchen97@gmail.com>
2026-07-19 18:12:44 +0300

    service-worker.js erstellen

---
+++ service-worker.js
@@ -0,0 +1,40 @@
+const CACHE_NAME = "sarahs-journey-v0.10";
+
+const urlsToCache = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./css/style.css",
+  "./js/app.js"
+];
+
+// Installation
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+// Dateien aus dem Cache laden
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
+
+// Alten Cache löschen
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(
+        keys
+          .filter((key) => key !== CACHE_NAME)
+          .map((key) => caches.delete(key))
+      )
+    )
+  );
+});
